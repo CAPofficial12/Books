93f323643075eabed96b86bb48321a5748781d34
Aarav <athakur@dohacollege.com.qa>
2026-04-06 05:49:25 +0300

    Start a time comparison between python and c++

--- C++/Basic/Beginning.cpp
+++ C++/Basic/Beginning.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <chrono>
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    int a = 0;
+    auto start = std::chrono::high_resolution_clock::now();
+    while (a < 10000000){
+        a++;
+    }
+
+   auto end = std::chrono::high_resolution_clock::now();
+
+   auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+   std::cout << "Time elapsed: " << diff.count() << " ms" << std::endl;
+
+   return 0;
 }
