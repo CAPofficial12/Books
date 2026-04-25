dd11ca65672c7c8ed56729fdaebb82364d03f6b1
Aarav <athakur@dohacollege.com.qa>
2026-04-25 22:39:30 +0300

    Attempted Hello triangle

---
+++ Physics Renders/src/Hello Triangle/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+
+int main(){
+    float vertices[] = {        //Sets positions of verticies of the triangle(x,y,z) between 1 and -1 (scales linearly)
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    return 0;
+}
